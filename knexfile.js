e8f9458571bd01903225a6f91256ccdef3463916
Ian Pessoa <ian.pessoaa@gmail.com>
2024-04-24 18:06:33 -0300

    criando estrutura da tabela de usuários

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+module.exports = {
+  test: {
+    client: 'pg',
+    version: '11',
+    connection: {
+      host: 'localhost',
+      user: 'admin',
+      password: '123456',
+      database: 'barrigaDB',
+    },
+    migrations: {
+      directory: 'src/migrations',
+    },
+  },
+};
